0b130bb233858d7df09c5dbc02a32005961112d0
Raztou3D <raztou3d@gmail.com>
2018-11-14 18:21:36 +0100

    copy env vairable to file for cron

--- start.sh
+++ start.sh
@@ -8,8 +8,10 @@ umount /dev/shm && mount -t tmpfs shm /dev/shm
 # easily control specific version dependency between your app and electron itself.
 # the syntax below starts an X istance with ONLY our electronJS fired up,
 # it saves you a LOT of resources avoiding full-desktops envs
-
 rm /tmp/.X0-lock &>/dev/null || true
+
+# Sharing env variables with cron
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /usr/src/app/container.env
 
 #run iFrame
 startx /usr/src/app/connectedframe.py
